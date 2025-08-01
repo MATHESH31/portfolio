8e526e9f364f118fe2134b435b5a6e63d2a341d2
mathezh1 <mathezh1@gmail.com>
2025-08-01 11:17:47 +0000

    legend is added in skills page

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import AppBar from '../app-bar/AppBar'
 import './Skills.css'
 import { Grid } from '@mui/joy'
+import { Chip } from '@mui/material'
 
 const Skills = () => {
     const leftGridStyling = {
@@ -29,6 +30,11 @@ const Skills = () => {
     return (
         <div className='skills-layout'>
             <AppBar />
+            <div className='legend'>
+                <h1>Categories</h1>
+                <Chip label='Have Experience' color='primary' component={'div'}/>
+                <Chip label='Learning in progress' color='error' component={'div'}/>
+            </div>
             <div className='skills-container'>
                 <div className='skills-header'>
                     <h1>
@@ -51,91 +57,91 @@ const Skills = () => {
                 <div className='skills-grid'>
                     <Grid container spacing={1} rowSpacing={3} sx={leftGridStyling}>
                         <Grid item xs={4}>
-                            <h1>
+                            <h1 className='learning'>
                                 HTML
                             </h1>
                         </Grid>
                         <Grid item xs={4}>
-                            <h1>
+                            <h1 className='learning'>
                                 CSS
                             </h1>
                         </Grid>
                         <Grid item xs={4}>
-                            <h1>
+                            <h1 className='learning'>
                                 JavaScript
                             </h1>
                         </Grid>
                         <Grid item xs={6}>
-                            <h1>
+                            <h1 className='learning'>
                                 Reactjs
                             </h1>
                         </Grid>
                         <Grid item xs={6}>
-                            <h1>
+                            <h1 className='learning'>
                                 Material UI
                             </h1>
                         </Grid>
                     </Grid>
                     <Grid container spacing={2} rowSpacing={3} sx={middleGridStyling}>
                         <Grid item xs={4}>
-                            <h1>
+                            <h1 className='experienced'>
                                 Docker
                             </h1>
                         </Grid>
                         <Grid item xs={4}>
-                            <h1>
+                            <h1 className='learning'>
                                 Kubernetes
                             </h1>
                         </Grid>
                         <Grid item xs={4}>
-                            <h1>
+                            <h1 className='experienced'>
                                 Linux
                             </h1>
                         </Grid>
                         <Grid item xs={6}>
-                            <h1>
+                            <h1 className='learning'>
                                 Apache Hadoop
                             </h1>
                         </Grid>
                         <Grid item xs={6}>
-                            <h1>
+                            <h1 className='learning'>
                                 Apache Spark
                             </h1>
                         </Grid>
                     </Grid>
                     <Grid container spacing={1} rowSpacing={3} sx={rightGridStyling}>
                             <Grid item xs={4}>
-                                <h1>
+                                <h1 className='experienced'>
                                     Core Java
                                 </h1>
                             </Grid>
                             <Grid item xs={4}>
-                                <h1>
+                                <h1 className='experienced'>
                                     Spring Boot
                                 </h1>
                             </Grid>
                             <Grid item xs={4}>
-                                <h1>
+                                <h1 className='experienced'>
                                     MongoDB
                                 </h1>
                             </Grid>
                             <Grid item xs={6}>
-                                <h1>
+                                <h1 className='experienced'>
                                     Spring Webflux
                                 </h1>
                             </Grid>
                             <Grid item xs={6}>
-                                <h1>
+                                <h1 className='experienced'>
                                     SQL
                                 </h1>
                             </Grid>
                             <Grid item xs={6}>
-                                <h1>
+                                <h1 className='experienced'>
                                     Kafka
                                 </h1>
                             </Grid>
                             <Grid item xs={6}>
-                                <h1>
+                                <h1 className='experienced'>
                                     Redis
                                 </h1>
                             </Grid>
